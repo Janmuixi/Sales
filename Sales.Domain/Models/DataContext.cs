65e010820ad41574cfad7648af5f3c65566e228f
janmuixi <janmuixi7@gmail.com>
2019-02-10 00:10:58 +0100

    creating datacontext

---
+++ Sales.Domain/Models/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Sales.Domain.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {}
+    }
+}
